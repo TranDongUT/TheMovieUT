629f13ecba4ab6db4cf2e3b6345b4caaadfe3df4
Huynh Hang <huynhhang1505@gmail.com>
2022-03-26 16:24:27 +0700

    fix heroSlide

--- src/components/HeroSlide.js
+++ src/components/HeroSlide.js
@@ -1,7 +1,9 @@
 import tmdbApi, { movieType } from "api/tmdbApi";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import SwiperCore, { Autoplay } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 function HeroSlide() {
   const [movieItems, setMovieItems] = useState([]);
@@ -21,24 +23,52 @@ function HeroSlide() {
   }, []);
 
   return (
-    <>
+    <div className="hero-slide">
       <Swiper
         modules={[Autoplay]}
         grabCursor={true}
         spaceBetween={0}
         slidesPerView={1}
+        autoplay={{ delay: 5000 }}
       >
-        {movieItems.map((item, i) => {
+        {movieItems.map((item, i) => (
           <SwiperSlide key={i}>
             {({ isActive }) => (
-              <img
-                src={`https://image.tmdb.org/t/p/original${item.backdrop_path}`}
+              <HeroSlideItem
+                item={item}
+                className={`${isActive ? "active" : ""}`}
               />
             )}
-          </SwiperSlide>;
-        })}
+          </SwiperSlide>
+        ))}
       </Swiper>
-    </>
+    </div>
+  );
+}
+
+function HeroSlideItem({ item }) {
+  const link = `/movie/${item.id}`;
+  return (
+    <div
+      className="slide-item"
+      style={{
+        backgroundImage: `url("http://image.tmdb.org/t/p/original/${item.backdrop_path}")`,
+      }}
+    >
+      <div className="container item-details">
+        <h2 className="item-title">{item.original_title}</h2>
+        <div className="item-desc">
+          <div className="item-vote">
+            <i className="bx bxs-star"></i>
+            {item.vote_average}
+          </div>
+          <div className="item-date">{item.release_date}</div>
+        </div>
+        <Link className="text-link" to={link}>
+          <button className="btn btn-trailer">Watch Now</button>
+        </Link>
+      </div>
+    </div>
   );
 }
 
